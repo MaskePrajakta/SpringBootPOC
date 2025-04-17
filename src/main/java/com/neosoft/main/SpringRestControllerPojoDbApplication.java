dc48a62dff69cbb8fbbcd66cdf143453d4741a6c
PrajaktaMaske <prajakta.maske@yahoo.com>
2025-04-17 11:52:34 +0530

    by Prajakta

--- src/main/java/com/neosoft/main/SpringRestControllerPojoDbApplication.java
+++ src/main/java/com/neosoft/main/SpringRestControllerPojoDbApplication.java
@@ -1,15 +1,8 @@
 package com.neosoft.main;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EntityScan
-@EnableJpaRepositories
-@ComponentScan
 public class SpringRestControllerPojoDbApplication {
 
 	public static void main(String[] args) {
